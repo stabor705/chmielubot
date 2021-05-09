711636f97f31c39fdb6598e002c60a999ce3101b
Bartosz Gwiazdowski <bartek05264@gmail.com>
2021-05-09 20:07:00 +0200

    major improvements

--- chmielubot/szkola.py
+++ chmielubot/szkola.py
@@ -5,6 +5,7 @@ from datetime import date
 import time
 import random
 
+
 class Szkola(commands.Cog):
     classes = {
         "4i2": "4i2"
@@ -58,11 +59,13 @@ class Szkola(commands.Cog):
             hours.append(hour)
             parts = lesson.find_all(class_='p n s'.split())
             groups = [parts[x*3:x*3+3] for x in range(len(parts) // 3)]
-            entry = tuple(' '.join([part.text.strip() for part in group]) for group in groups)
+            entry = tuple(' '.join([part.text.strip()
+                          for part in group]) for group in groups)
             if not entry:
                 entry = tuple(" ")
             entries.append(entry)
-        max_line_length = max([len(entry[0]) + len(hour) + 5 for entry, hour in zip(entries, hours)])
+        max_line_length = max(
+            [len(entry[0]) + len(hour) + 5 for entry, hour in zip(entries, hours)])
         stars_length = (max_line_length - len(self.days[day])) // 2
         res = "-" * stars_length + self.days[day] + "-" * stars_length + "\n"
         for idx, hour, entry in zip(range(len(entries)), hours, entries):
@@ -75,9 +78,19 @@ class Szkola(commands.Cog):
 
     @commands.command(name="matura", help="Pokazuje, ile dni pozostalo do matury")
     async def matura(self, ctx):
-       exam = date(2021, 5, 4)
-       school_end = date(2021, 4, 30)
-       today = date.today()
-       time1 = (exam - today)
-       time2 = (school_end - today)
-       await ctx.send(f"```css\nDo końca roku szkolnego pozostało: {time2.days} dni\nDo matury pozostało: {time1.days} dni```")
+        exam = date(2021, 5, 4)
+        school_end = date(2021, 4, 30)
+        today = date.today()
+        time1 = (exam - today)
+        time2 = (school_end - today)
+        if time2.days < 0:
+            outstr = f"Od końca roku szkolnego upłynęło: {abs(time2.days)} dni\n"
+        else: 
+            outstr = f"Do końca roku szkolnego pozostało: {time2.days} dni\n"
+    
+        if time1.days < 0:
+            outstr += f"Od rozpoczęcia matur upłynęło: {abs(time1.days)} dni"
+        else:
+            outstr += f"Do rozpoczęcia matur pozostało: {time1.days} dni"
+       # await ctx.send(f"```css\nDo końca roku szkolnego pozostało: {time2.days} dni\nDo matury pozostało: {time1.days} dni```")
+        await ctx.send(f"```css\n{outstr}```")
